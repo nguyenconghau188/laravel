d8660f801128cf065c718982bac432b7940eb866
Nguyen Cong Hau <nguyenconghau188@gmail.com>
2018-08-14 23:20:10 +0700

    +add model

--- MySpecialWeb/app/User.php
+++ MySpecialWeb/app/User.php
@@ -26,4 +26,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function comment()
+    {
+        return $this->hasMany('App\Comment', 'idUser', 'id');
+    }
 }

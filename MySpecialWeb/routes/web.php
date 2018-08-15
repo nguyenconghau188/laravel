83ab94eab1686410fa13393c8003bed9c8b8d867
Nguyen Cong Hau <nguyenconghau188@gmail.com>
2018-08-15 09:17:52 +0700

    *add soem routes

--- MySpecialWeb/routes/web.php
+++ MySpecialWeb/routes/web.php
@@ -14,3 +14,40 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('test_view', function(){
+		return view('admin.theloai.danhsach');
+});
+
+
+Route::group(['prefix'=>'admin'], function(){
+	Route::group(['prefix'=>'theloai'], function(){
+		Route::get('danhsach', 'TheLoaiController@getDanhSach');
+		Route::get('sua', 'TheLoaiController@getSua');
+		Route::get('them', 'TheLoaiController@getThem');
+	});
+
+	Route::group(['prefix'=>'loaitin'], function(){
+		Route::get('danhsach', 'LoaiTinController@getDanhSach');
+		Route::get('sua', 'LoaiTinController@getSua');
+		Route::get('them', 'LoaiTinController@getThem');
+	});
+
+	Route::group(['prefix'=>'comment'], function(){
+		Route::get('danhsach', 'CommentController@getDanhSach');
+		Route::get('sua', 'CommentController@getSua');
+		Route::get('them', 'CommentController@getThem');
+	});
+
+	Route::group(['prefix'=>'tintuc'], function(){
+		Route::get('danhsach', 'TinTucController@getDanhSach');
+		Route::get('sua', 'TinTucController@getSua');
+		Route::get('them', 'TinTucController@getThem');
+	});
+
+	Route::group(['prefix'=>'user'], function(){
+		Route::get('danhsach', 'UserController@getDanhSach');
+		Route::get('sua', 'UserController@getSua');
+		Route::get('them', 'UserController@getThem');
+	});
+});

2c4d76ea44459e4ac8d1de31a5cd71515a9a2fde
Nguyen Cong Hau <nguyenconghau188@gmail.com>
2018-08-26 23:56:14 +0700

    !add project MVC

--- websiteMVC/system/config/config.php
+++ websiteMVC/system/config/config.php
@@ -1,3 +1,10 @@
 <?php 
 //chứa các hằng số định nghĩa sử dụng trong hệ thống chính
+define('DB_HOST', "localhost");
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+
+define('CONTROLLER_DEFAULT', 'index');
+define('ACTION_DEFAULT', 'index');
+
 ?>

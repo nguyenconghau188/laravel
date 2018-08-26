4273e739c2679c6046e5a278d0c94283d5ee1e6b
Nguyen Cong Hau <nguyenconghau188@gmail.com>
2018-08-27 00:18:45 +0700

    *add load action

--- websiteMVC/system/core/FT_Controller.php
+++ websiteMVC/system/core/FT_Controller.php
@@ -29,7 +29,25 @@ class FT_Controller
 
     public function load($controller, $action)
     {
-    	
+		//chuyển đổi tên controller vì mặc định nó có định dạng là {Name}_Controller
+		$controller = ucfirst(strtolower($controller)).'_Controller';
+		//hàm ucfirst-> chuyển đổi kí tự đầu tiên trong chuỗi thành in hoa nếu đó là 1 chữ cái
+		$action = strtolower($action).'Action';
+
+		if (!file_exists(PATH_APPLICATION.'/controller'.$controller.'.php')) {
+		    	die('Controller không tồn tại');
+		}
+		require_once PATH_APPLICATION.'/controller'.$controller.'.php';
+		if (!class_exists($controller)) {
+		    die('Controller không tồn tại');
+		}    	
+
+		$controllerObject = new $controller();
+		if (!method_exists($controllerObject, $action)) {
+			die('Action không tồn tại');
+		}
+
+		$controllerObject->{$action};
     }
 }
  ?>

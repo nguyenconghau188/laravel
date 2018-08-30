504aafc1d1299ef3a8d23480a71cc6e7fabbb11a
Nguyen Cong Hau <nguyenconghau188@gmail.com>
2018-08-30 15:21:11 +0700

    *test

--- websiteMVC/admin/controller/News_Controller.php
+++ websiteMVC/admin/controller/News_Controller.php
@@ -7,9 +7,16 @@ class News_Controller extends FT_Controller
 {
 	public function indexAction()
 	{
-		echo '<pre>';
-		echo '<h1>Index Action</h1>';
-		print_r($this);
+		// lấy config có key là csrf_token_name
+		echo '<h3>Token: scrf_token_name: '.$this->config->item('csrf_token_name').'</h3>';
+
+		//thay đổi giá trị cho csrf_token_name
+		$this->config->setItem('csrf_token_name', 'new_token');
+		echo '<h3>Token: csrf_token_name (changed): '.$this->config->item('csrf_token_name').'</h3>';
+
+		//tạo cấu hình mới trên website name
+		$this->config->setItem('website_name', 'freetuts.net');
+		echo '<h3>key website_name: '.$this->config->item('website_name').'</h3>';
 	}
 
 	public function detailAction()

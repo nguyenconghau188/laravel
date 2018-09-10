e63d5440d5656a41ec2191bfc2c90679ce034122
Nguyen Cong Hau <nguyenconghau188@gmail.com>
2018-09-10 13:08:45 +0700

    *add init

---
+++ inote/core/init.php
@@ -0,0 +1,21 @@
+<?php
+require_once 'class/DB.php';
+require_once 'class/Session.php';
+
+$db = new DB();
+$db->connect();
+
+$session = new Session();
+$session->start();
+$user = $session->get();
+
+date_default_timezone_set('Asia/Ho_Chi_Minh');
+$date_current = '';
+$date_current = date("Y-m-d H:i:sa");
+
+if ($user) {
+	$sql_get_date_user = "SELECT * FROM users WHERE username = '$username'";
+	$data_user = $db->fetch_assoc($sql_get_date_user, 1);
+}
+ 
+?>
